139a8895dd72da06b3be87ec22f54ee5b0e1d78e
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-24 16:25:46 +0100

    search recipes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const recipeResult = document.getElementById("recipe-result")
 const searchButton = document.getElementById("search-button")
-const userInput = document.getElementById("user-input").value
 
 const url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
@@ -12,48 +11,81 @@ const url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 //     console.log(recipeResult)
     
 // }
-fetch(url + "pizza")
-    .then((Response)=> Response.json())
-    .then((data) => {
-        // console.log(data)
-        let searchedMeal = data.meals[0]
-        console.log(searchedMeal)
-        console.log("strMeal:",searchedMeal.strMeal)
-        console.log("strArea:", searchedMeal.strArea)
-        console.log("strMealThumb:", searchedMeal.strMealThumb)
-        console.log("strInstructions", searchedMeal.strInstructions)
-        let count = 1
-        let ingredients = []
-        for(let i in searchedMeal) {
-            // console.log("i:", i)
-            let ingredient = ""
-            let measure = ""
-            if(i.startsWith("strIngredient") && searchedMeal[i]) {
-                // console.log("searchedMeal[i]",searchedMeal[i])
-                ingredient = searchedMeal[i]
-                measure =searchedMeal[`strMeasure` + count]
-                count += 1
-                ingredients.push(`${measure} ${ingredient}`)
-            }
-        }
-        
+
+searchButton.addEventListener('click', () => {
+    let userInput = document.getElementById("user-input").value;
+    if(userInput.length == 0) {
         recipeResult.innerHTML = `
-        <img src=${searchedMeal.strMealThumb} />
-        <div class="recipe-details">
-            <h2>${searchedMeal.strMeal}</h2>
-            <h4>${searchedMeal.strArea}</h4>
-        </div>
-        <div id="ingredients-container">
-            <div id="recipe">
-                <button id="hide-recipe">X</button>
-                <pre id="instructions">${searchedMeal.strInstructions}</pre>
-            </div>
-            <button id="show-recipe">View Recipe</button>
-        </div>
+        <h3>You Must Enter a Recipe</h3>
         `
-        const ingredientsContainer = document.getElementById("ingrediants-container")
-        let parent = document.createElement("ul")
-        const recipe = document.getElementById("recipe")
-        const hideRecipe = document.getElementById("hide-recipe")
-        const showRecipe = document.getElementById("show-recipe")
-    })
+    }
+    else {
+        fetch(url + userInput)
+        .then((Response)=> Response.json())
+        .then((data) => {
+            // console.log(data)
+            let searchedMeal = data.meals[0]
+            console.log(searchedMeal)
+            console.log("strMeal:",searchedMeal.strMeal)
+            console.log("strArea:", searchedMeal.strArea)
+            console.log("strMealThumb:", searchedMeal.strMealThumb)
+            console.log("strInstructions", searchedMeal.strInstructions)
+            let count = 1
+            let ingredients = []
+            for(let i in searchedMeal) {
+                // console.log("i:", i)
+                let ingredient = ""
+                let measure = ""
+                if(i.startsWith("strIngredient") && searchedMeal[i]) {
+                    // console.log("searchedMeal[i]",searchedMeal[i])
+                    ingredient = searchedMeal[i]
+                    measure =searchedMeal[`strMeasure` + count]
+                    count += 1
+                    ingredients.push(`${measure} ${ingredient}`)
+                }
+            }
+            
+            recipeResult.innerHTML = `
+            <img src=${searchedMeal.strMealThumb} />
+            <div class="recipe-details">
+                <h2>${searchedMeal.strMeal}</h2>
+                <h4>${searchedMeal.strArea}</h4>
+            </div>
+            <div id="ingredients-container"></div>
+                <div id="recipe">
+                    <button id="hide-recipe">X</button>
+                    <pre id="instructions">${searchedMeal.strInstructions}</pre>
+                </div>
+                <button id="show-recipe">View Recipe</button>
+            `
+            const ingredientsContainer = document.getElementById("ingredients-container")
+            let parent = document.createElement("ul")
+            const recipe = document.getElementById("recipe")
+            const hideRecipe = document.getElementById("hide-recipe")
+            const showRecipe = document.getElementById("show-recipe")
+
+            ingredients.forEach((ingredient => {
+                let child = document.createElement("li")
+                child.innerText = ingredient;
+                parent.appendChild(child)
+                ingredientsContainer.appendChild(parent)
+            }));
+
+            hideRecipe.addEventListener("click", () => {
+                console.log("click")
+                recipe.style.display = "none";
+            })
+
+            showRecipe.addEventListener("click", () => {
+                console.log("click")
+                recipe.style.display = "block"
+            })
+        }).catch (() => {
+            recipeResult.innerHTML = `
+            <h3>You must Enter a Valid Recipe</h3>
+            `
+        })
+
+    }
+})
+
